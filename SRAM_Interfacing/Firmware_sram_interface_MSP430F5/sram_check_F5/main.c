726c68f118e7f7ac9d39956745b01d494400a5ad
HABIB UR RAHMAN <78023625+habib1710001@users.noreply.github.com>
2024-09-29 01:24:00 -0600

    Include_ram_power_off_feature

--- SRAM_Interfacing/Firmware_sram_interface_MSP430F5/sram_check_F5/main.c
+++ SRAM_Interfacing/Firmware_sram_interface_MSP430F5/sram_check_F5/main.c
@@ -21,8 +21,19 @@
 #include <msp430.h> 
 #include <stdint.h>
 
-#define TARGET_MEMORY_START_ADDRESS0 ((uint8_t*)0x01C6C)
-#define TARGET_MEMORY_START_ADDRESS1 ((uint8_t*)0x01D20)
+#define TARGET_MEMORY_START_ADDRESS0 ((uint8_t*)0x003500)
+#define TARGET_MEMORY_START_ADDRESS1 ((uint8_t*)0x00356C)
+
+
+#define RCCTL0_BASE_ADDRESS 0x000158
+#define RCCTL0_OFFSET_ADDRESS 0x00
+
+#define RCCTL0 *((uint16_t*)(RCCTL0_OFFSET_ADDRESS + RCCTL0_BASE_ADDRESS))
+
+#define RCKEY    (0x5A00)
+#define RCRS2OFF (0x0004)
+#define RCRS2ON  (0x0000)
+
 
 #define BYTE_BLOCK0 30
 #define BYTE_BLOCK1 20
@@ -43,15 +54,23 @@ void memory_continuous_write (uint8_t* startingAddress, uint8_t number_of_bytes,
 }
 
 
+void ram_turn_off(){
+    RCCTL0 = (RCKEY | RCRS2OFF);
+    delay(10000);
+    RCCTL0 = (RCKEY | RCRS2ON);
+    delay(10000);
+}
+
+
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
 
     while(1){
-
-        delay(10000);
-
         memory_continuous_write (TARGET_MEMORY_START_ADDRESS0, BYTE_BLOCK0, 0x00);
+
+        ram_turn_off();
+
         memory_continuous_write (TARGET_MEMORY_START_ADDRESS1, BYTE_BLOCK1, 0x11);
     }
 }
