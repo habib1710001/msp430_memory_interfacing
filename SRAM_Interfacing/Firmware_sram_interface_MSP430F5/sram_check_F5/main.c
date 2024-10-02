8651253ca49c76625597c6114ca9a237eb37840e
HABIB UR RAHMAN <78023625+habib1710001@users.noreply.github.com>
2024-10-02 16:12:23 -0600

    Added_timer_control

--- SRAM_Interfacing/Firmware_sram_interface_MSP430F5/sram_check_F5/main.c
+++ SRAM_Interfacing/Firmware_sram_interface_MSP430F5/sram_check_F5/main.c
@@ -21,7 +21,7 @@
 #include <msp430.h> 
 #include <stdint.h>
 
-#define TARGET_MEMORY_START_ADDRESS0 ((uint8_t*)0x003500)
+#define TARGET_MEMORY_START_ADDRESS0 ((uint8_t*)0x003400)
 #define TARGET_MEMORY_START_ADDRESS1 ((uint8_t*)0x00356C)
 
 
@@ -35,18 +35,19 @@
 #define RCRS2ON  (0x0000)
 
 
-#define BYTE_BLOCK0 30
+#define BYTE_BLOCK0 1024
 #define BYTE_BLOCK1 20
 
+#define TIME_DISABLED_CHEKCK0_MS 1
+#define TIME_DISABLED_CHEKCK1_MS 18
+#define TIME_DISABLED_CHEKCK2_MS 20
+#define TIME_DISABLED_CHEKCK3_MS 22
+#define TIME_DISABLED_CHEKCK4_MS 23
+#define TIME_DISABLED_CHEKCK5_MS 48
 
-void delay (uint32_t delayIteration){
-    volatile uint32_t j;
-    for (j = 1; j < delayIteration; j++){
-      //looping delay
-    }
-}
 
-void memory_continuous_write (uint8_t* startingAddress, uint8_t number_of_bytes, uint8_t content){
+
+void memory_continuous_write (uint8_t* startingAddress, uint16_t number_of_bytes, uint8_t content){
     uint16_t i;
     for (i = 0; i < number_of_bytes; i++){
       *(startingAddress + i) = content;
@@ -54,23 +55,54 @@ void memory_continuous_write (uint8_t* startingAddress, uint8_t number_of_bytes,
 }
 
 
-void ram_turn_off(){
+void init_timer(void){
+    //Timer Configuration
+    TA0CCR0 = 0; //Initially stop the timer
+    TA0CCTL0 |= CCIE ; //Enable the interrupt for CCR0
+    TA0CTL = (TASSEL_2 +  ID_0 + MC_1 + TAIE); //Select the SMCLK, SMCLK/, Up Mode
+}
+
+
+void ram_turn_on_after(uint8_t targetMs){
+    TA0CCR0 = 1000-1; //Start Timer, Compare value for Up Mode to get 1ms delay per loop
     RCCTL0 = (RCKEY | RCRS2OFF);
-    delay(10000);
-    RCCTL0 = (RCKEY | RCRS2ON);
-    delay(10000);
+    uint8_t countMs = 0;
+
+    while (1){
+      if (TA0CCTL0 && CCIFG){
+          countMs++;
+      }
+      if (countMs >= targetMs){
+       RCCTL0 = (RCKEY | RCRS2ON);
+       countMs = 0;
+       break;
+     }
+    }
 }
 
 
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    init_timer();
 
     while(1){
-        memory_continuous_write (TARGET_MEMORY_START_ADDRESS0, BYTE_BLOCK0, 0x00);
+        memory_continuous_write (TARGET_MEMORY_START_ADDRESS0, BYTE_BLOCK0, 0xFF);
+        ram_turn_on_after(TIME_DISABLED_CHEKCK0_MS);
 
-        ram_turn_off();
+        memory_continuous_write (TARGET_MEMORY_START_ADDRESS0, BYTE_BLOCK0, 0xFF);
+        ram_turn_on_after(TIME_DISABLED_CHEKCK1_MS);
 
-        memory_continuous_write (TARGET_MEMORY_START_ADDRESS1, BYTE_BLOCK1, 0x11);
+        memory_continuous_write (TARGET_MEMORY_START_ADDRESS0, BYTE_BLOCK0, 0xFF);
+        ram_turn_on_after(TIME_DISABLED_CHEKCK2_MS);
+
+        memory_continuous_write (TARGET_MEMORY_START_ADDRESS0, BYTE_BLOCK0, 0xFF);
+        ram_turn_on_after(TIME_DISABLED_CHEKCK3_MS);
+
+        memory_continuous_write (TARGET_MEMORY_START_ADDRESS0, BYTE_BLOCK0, 0xFF);
+        ram_turn_on_after(TIME_DISABLED_CHEKCK4_MS);
+
+        memory_continuous_write (TARGET_MEMORY_START_ADDRESS0, BYTE_BLOCK0, 0xFF);
+        ram_turn_on_after(TIME_DISABLED_CHEKCK4_MS);
     }
 }
